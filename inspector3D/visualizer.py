6cd5277f966b15662d44443db1c391fe46a2a7cc
Jonas Kneifl <jonas.kneifl@itm.uni-stuttgart.de>
2023-06-02 14:41:10 +0200

    transparent backround as default

--- inspector3D/visualizer.py
+++ inspector3D/visualizer.py
@@ -14,12 +14,13 @@ import matplotlib.colors as mcolors
 
 
 class Visualizer(object):
-    def __init__(self, frames_per_sec: float = 20, background_color: str = 'w', grid: bool = False,
+    def __init__(self, frames_per_sec: float = 20, background_color=[1, 1, 1, 0], grid: bool = False,
                  resolution: list = [2560, 1600]):
         '''
         Visualizer for scatter plots and mesh items. It can be used to animate and compare simulations.
         :param frames_per_sec: {float} default: 20, amount of visualized frames per second
-        :param background_color: {str} default: 'w'', string describing a color in which the background is drawn
+        :param background_color:  default: [1, 1, 1, 0] (transparent), string or list describing a color in which the
+            background is drawn
         :param grid: {bool} default: False, if true a grid is drawn in the animation
         :param resolution: {list} default: [2560, 1600], resolution of the animation window
         '''
@@ -361,31 +362,33 @@ class Visualizer(object):
                                                                color = gl_Color;
                                                                gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;  
                                                             }"""),
-                                 gl.shaders.FragmentShader("""varying vec3 vN;
-                                                                varying vec3 v; 
+                                 gl.shaders.FragmentShader(
+                                     """varying vec3 vN;
+                                                                varying vec3 v;
                                                                 varying vec4 color;
-                                                                #define MAX_LIGHTS 1 
-                                                                void main (void) 
-                                                                { 
+                                                                #define MAX_LIGHTS 1
+                                                                void main (void)
+                                                                {
                                                                    vec3 N = normalize(vN);
-                                                                   vec4 finalColor = vec4(0.0, 0.0, 0.0, 0.0);
-                                                                   
+                                                                   vec4 finalColor = vec4(0.1, 0.1, 0.1, 0.0);
+
                                                                    for (int i=0;i<MAX_LIGHTS;i++)
                                                                    {
-                                                                      vec3 L = normalize(gl_LightSource[i].position.xyz - v); 
-                                                                      vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0) 
-                                                                      vec3 R = normalize(-reflect(L,N)); 
-                                                                   
-                                                                      vec4 Iamb = gl_LightSource[i].ambient; 
+                                                                      vec3 L = normalize(gl_LightSource[i].position.xyz - v);
+                                                                      vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0)
+                                                                      vec3 R = normalize(-reflect(L,N));
+
+                                                                      vec4 Iamb = gl_LightSource[i].ambient;
                                                                       vec4 Idiff = gl_LightSource[i].diffuse * max(dot(N,L), 0.0);
-                                                                      Idiff = clamp(Idiff, 0.0, 1.0); 
+                                                                      Idiff = clamp(Idiff, 0.0, 1.0);
                                                                       vec4 Ispec = gl_LightSource[i].specular * pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
-                                                                      Ispec = clamp(Ispec, 0.0, 1.0); 
-   
-                                                                      finalColor += Iamb + Idiff + 0.01*Ispec;
+                                                                      Ispec = clamp(Ispec, 0.0, 1.0);
+
+                                                                      finalColor += 1.00*Iamb + 1.00*Idiff + 0.01*Ispec;
                                                                    }
-                                                                   gl_FragColor = color * finalColor; 
-                                                                }""")
+                                                                   gl_FragColor = color * finalColor;
+                                                                }"""
+                                                           )
                                  ]))
 
                 sp = pg.opengl.GLMeshItem(
